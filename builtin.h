0bed703dc9b4814a0e56eab30a71d0a01be014cd
gaolidong_aa@sina.com <gaolidong_aa@sina.com>
2018-12-23 11:55:09 +0800

    [Reason] change builtin.h [Name]gaolidong [Date]20181228 [Test]

--- builtin.h
+++ builtin.h
@@ -7,74 +7,74 @@
 #include "commit.h"
 
 /*
- * builtin API
+ * builtin API - 内置API
  * ===========
  *
- * Adding a new built-in
+ * Adding a new built-in -  增加一个新的内置命令
  * ---------------------
  *
  * There are 4 things to do to add a built-in command implementation to
  * Git:
- *
+ * - git 增加一个内置命令需要完成4个步骤
  * . Define the implementation of the built-in command `foo` with
  *   signature:
- *
+ * - 定义内置函数FOO的声明，如下
  *	int cmd_foo(int argc, const char **argv, const char *prefix);
  *
  * . Add the external declaration for the function to `builtin.h`.
- *
+ * - 在 builtin.h 中，添加扩展声明
  * . Add the command to the `commands[]` table defined in `git.c`.
  *   The entry should look like:
- *
+ * - 在git.c 文件中，给结构体中，添加命令结构体，形式如下：
  *	{ "foo", cmd_foo, <options> },
  *
  * where options is the bitwise-or of:
- *
- * `RUN_SETUP`:
+ *- options 选项是按位与获得：
+ * `RUN_SETUP`: - 运行时配置信息
  *	If there is not a Git directory to work on, abort.  If there
  *	is a work tree, chdir to the top of it if the command was
  *	invoked in a subdirectory.  If there is no work tree, no
  *	chdir() is done.
- *
- * `RUN_SETUP_GENTLY`:
+ * - 如果没有git 工作路径，不执行；如果有git,自动调整到最外层路径执行。没有工作，不执行 chdir
+ * `RUN_SETUP_GENTLY`: - 一般运行时配置
  *	If there is a Git directory, chdir as per RUN_SETUP, otherwise,
  *	don't chdir anywhere.
  *
  * `USE_PAGER`:
- *
+ * - 使用页面调度器
  *	If the standard output is connected to a tty, spawn a pager and
  *	feed our output to it.
- *
+ * - 如果标准输出链接到TTY，要产生一个页面调度器并且使我们的输出适应tty
  * `NEED_WORK_TREE`:
- *
+ * - 需要工作树
  *	Make sure there is a work tree, i.e. the command cannot act
  *	on bare repositories.
  *	This only makes sense when `RUN_SETUP` is also set.
- *
+ * - 确认工作树存在，说明命令不能在空仓库时执行, 只有与RUN_SETUP一起配置时才有用。
  * `SUPPORT_SUPER_PREFIX`:
- *
+ * - 支持超级前缀
  *	The built-in supports `--super-prefix`.
  *
  * `DELAY_PAGER_CONFIG`:
- *
+ * - 延迟页面适配器配置
  *	If RUN_SETUP or RUN_SETUP_GENTLY is set, git.c normally handles
  *	the `pager.<cmd>`-configuration. If this flag is used, git.c
  *	will skip that step, instead allowing the built-in to make a
  *	more informed decision, e.g., by ignoring `pager.<cmd>` for
  *	certain subcommands.
- *
+ * - 配置了RUN SETUP 或者 RUN SETUP GENTLY时，git.c 一般会处理 pager.cmd -的配置信息，如果其用这个配置，git.c 会忽略上述动作，允许内置更准确的内置信息。
  * . Add `builtin/foo.o` to `BUILTIN_OBJS` in `Makefile`.
- *
+ * - 在 Makefile 中 添加 builtin/foo.h
  * Additionally, if `foo` is a new command, there are 4 more things to do:
- *
+ * 另外，foo 是个新命令，有4个事情需要去做。
  * . Add tests to `t/` directory.
- *
+ *   增加 测试信息到 t/ 目录中
  * . Write documentation in `Documentation/git-foo.txt`.
- *
+ *   写相关介绍信息到 Documengtation/git-foo.txt
  * . Add an entry for `git-foo` to `command-list.txt`.
- *
+ *   command-list 添加 git-foo
  * . Add an entry for `/git-foo` to `.gitignore`.
- *
+ *   /git-foo 添加到 .gitignore中
  *
  * How a built-in is called
  * ------------------------
